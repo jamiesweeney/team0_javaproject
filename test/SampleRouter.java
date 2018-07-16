87a26be8789487594d5e38e7708df81e4e5783a1
OliverMorrison <oliverjaymorrison@gmail.com>
2018-07-16 10:59:02 +0100

    quick code cleanup

--- test/SampleRouter.java
+++ test/SampleRouter.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 import javax.net.ServerSocketFactory;
 
-import OrderManager.Order;
 import OrderRouter.Router;
 import Ref.Instrument;
 import Ref.Ric;
@@ -29,50 +28,89 @@ public class SampleRouter extends Thread implements Router
 		this.setName(name);
 		this.port=port;
 	}
+
 	ObjectInputStream is;
 	ObjectOutputStream os;
-	public void run(){
-		//OM will connect to us
-		try {
+
+	//Called when thread is started
+	public void run()
+    {
+		//OrderManager will connect to us
+		try
+        {
 			omConn=ServerSocketFactory.getDefault().createServerSocket(port).accept();
-			while(true){
-				if(0<omConn.getInputStream().available()){
+
+			while(true)
+            {
+				if(0<omConn.getInputStream().available())
+				{
 					is=new ObjectInputStream(omConn.getInputStream());
 					Router.api methodName=(Router.api)is.readObject();
 					System.out.println("Order Router recieved method call for:"+methodName);
-					switch(methodName){
+					switch(methodName)
+                    {
 						case routeOrder:routeOrder(is.readInt(),is.readInt(),is.readInt(),(Instrument)is.readObject());break;
 						case priceAtSize:priceAtSize(is.readInt(),is.readInt(),(Instrument)is.readObject(),is.readInt());break;
 					}
-				}else{
+				}
+				else
+				{
 					Thread.sleep(100);
 				}
 			}
-		} catch (IOException | ClassNotFoundException | InterruptedException e) {
+		}
+		catch (IOException | ClassNotFoundException | InterruptedException e)
+        {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+
 	@Override
-	public void routeOrder(int id,int sliceId,int size,Instrument i) throws IOException, InterruptedException{ //MockI.show(""+order);
+	public void routeOrder(int id,
+                           int sliceId,
+                           int size,
+                           Instrument i) throws IOException, InterruptedException
+    { //MockI.show(""+order);
 		int fillSize=RANDOM_NUM_GENERATOR.nextInt(size);
+
 		//TODO have this similar to the market price of the instrument
 		double fillPrice=199*RANDOM_NUM_GENERATOR.nextDouble();
+
 		Thread.sleep(42);
-		os=new ObjectOutputStream(omConn.getOutputStream());
+
+		os = new ObjectOutputStream(omConn.getOutputStream());
+
 		os.writeObject("newFill");
+
 		os.writeInt(id);
+
 		os.writeInt(sliceId);
+
 		os.writeInt(fillSize);
+
 		os.writeDouble(fillPrice);
+
 		os.flush();
 	}
 
 	@Override
-	public void sendCancel(int id,int sliceId,int size,Instrument i){ //MockI.show(""+order);
+    public void sendCancel(int id,
+                           int sliceId,
+                           int size,
+                           Instrument i)
+    {
+        //MockI.show(""+order);
 	}
+
+
 	@Override
-	public void priceAtSize(int id, int sliceId,Instrument i, int size) throws IOException{
+	public void priceAtSize(int id,
+                            int sliceId,
+                            Instrument i,
+                            int size) throws IOException
+    {
 		os=new ObjectOutputStream(omConn.getOutputStream());
 		os.writeObject("bestPrice");
 		os.writeInt(id);
