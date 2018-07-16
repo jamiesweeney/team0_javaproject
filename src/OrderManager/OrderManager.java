46024be6321a365a5fe3976ba8ccff020c390146
Ryan Porteous <porteous96@hotmail.co.uk>
2018-07-16 12:15:43 +0100

    Renamed a variable to be camelCase

--- src/OrderManager/OrderManager.java
+++ src/OrderManager/OrderManager.java
@@ -5,17 +5,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.IntSummaryStatistics;
 import java.util.Map;
 
 import Database.Database;
 import LiveMarketData.LiveMarketData;
 import OrderClient.NewOrderSingle;
 import OrderRouter.Router;
-import OrderRouter.Router.api;
 import TradeScreen.TradeScreen;
 
 public class OrderManager {
@@ -142,7 +138,7 @@ public class OrderManager {
 		ObjectOutputStream os=new ObjectOutputStream(clients[o.clientid].getOutputStream());
 		//newOrderSingle acknowledgement
 		//ClOrdId is 11=
-		os.writeObject("11="+o.ClientOrderID+";35=A;39=0");
+		os.writeObject("11="+o.clientOrderID +";35=A;39=0");
 		os.flush();
 
 		price(id,o);
