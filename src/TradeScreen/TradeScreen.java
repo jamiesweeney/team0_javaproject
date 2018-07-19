7d1c761077812ba53a965738bd080b426a26b36d
jamiesweeney <321.jamiesweeney.321@gmail.com>
2018-07-19 11:04:37 +0100

    Trade scren interface javadocs

--- src/TradeScreen/TradeScreen.java
+++ src/TradeScreen/TradeScreen.java
@@ -6,7 +6,21 @@ import OrderManager.Order;
 
 public interface TradeScreen {
 	enum api{newOrder,price,fill,cross};
+
+	/** Sends a new order to the trade screen.
+	 *
+	 * @param id the id of the OM order
+	 * @param order the order object itself
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	void newOrder(int id,Order order) throws IOException, InterruptedException;
+
+	/** 
+	 *
+	 * @param id
+	 * @throws IOException
+	 */
 	void acceptOrder(int id) throws IOException;
 	void sliceOrder(int id,int sliceSize) throws IOException;
 	void price(int id,Order o) throws InterruptedException, IOException;
