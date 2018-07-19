7e371325326a5a210decaac681796907f813c1b2
jamiesweeney <321.jamiesweeney.321@gmail.com>
2018-07-19 10:35:09 +0100

    javadocs for the order job class

--- src/OrderManager/OrderJob.java
+++ src/OrderManager/OrderJob.java
@@ -3,6 +3,11 @@ package OrderManager;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/** Contains the information for a new client order job. the method
+ * that the job has to do and the arguments that it uses
+ *
+ */
 public class OrderJob {
 
     String method;
